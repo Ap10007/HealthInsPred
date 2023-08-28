2e6fe28f5b9562e1f5cc48abf5c0a0139cb57f6e
Ap10007 <anuj0000000003@gmail.com>
2023-08-29 01:14:24 +0530

    Model Trainer commit

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -25,7 +25,7 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            df=pd.read_csv('notebook\data/insurance.csv')
+            df=pd.read_csv('notebook\data\insurance.csv')
             logging.info('Read the dataset as dataframe')
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
@@ -51,9 +51,11 @@ class DataIngestion:
         
 if __name__=="__main__":
     obj=DataIngestion()
+    # obj.initiate_data_ingestion()
     train_data,test_data=obj.initiate_data_ingestion()
 
     data_transformation=DataTransformation()
+    #data_transformation.initiate_data_transformation(train_data,test_data)
     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
 
     modeltrainer=ModelTrainer()
